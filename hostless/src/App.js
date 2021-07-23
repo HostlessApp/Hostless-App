1c7a8e606b91cda2d9ca4848eaa76d681a7edba0
kevin-m <kmclaughlinbtb@gmail.com>
2021-07-23 18:57:35 -0500

    added user creation form and api post

--- hostless/src/App.js
+++ hostless/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import AddRestaurant from "./components/Restaurant/AddRestaurant";
+import AddUser from "./components/AddUser";
 import axios from "axios";
 
 function App() {
@@ -10,10 +11,18 @@ function App() {
     axios.post("http://localhost:3000/restaurants", payload).then((res) => console.log(res));
   }
 
+  function createUser(data) {
+    const payload = data;
+    console.log(payload)
+    axios.post('http://localhost:3000/users', payload)
+      .then((res) => console.log(res));
+  }
 
   return (
     <div className="App">
       <AddRestaurant createRestaurant={createRestaurant} />
+      <hr />
+      <AddUser createUser={createUser} />
       {/* <main>
             <Route />
         </main> */}
